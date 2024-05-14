3fc048188e1cb966216a1a46ba205db2fadf86e2
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-05-15 01:58:59 +0500

    Add some Function

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const getStyleName = btn => {
+  const className = {
+    '=': 'equals',
+    'x': 'opt',
+    '-': 'opt',
+    '+': 'opt',
+    '/': 'opt',
+  }
+  return className[btn]
+}
+const Button = ({ value }) => {
+  return (
+    <div className={`${getStyleName(value)} button `}>{value}</div>
+  )
+}
+
+export default Button
---
+++ src/components/Screen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Screen = () => {
+  return (
+    <div className='screen'>66666687</div>
+  )
+}
+
+export default Screen
