08b21c76e1bf1ed98b92dd4a28a2a50245bd7172
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-05-15 01:18:16 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Wrapper from './components/Wrapper'
+
+const App = () => {
+  return (
+   <Wrapper>
+    <h1>App</h1>
+
+   </Wrapper>
+  )
+}
+
+export default App
