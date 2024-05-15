d29ecd996545b5f9f26d7a7a4548b8b6ecf318dc
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-05-16 01:49:58 +0500

    Complete the Project

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,61 +12,100 @@ const getStyleName = (btn) => {
 };
 const Button = ({ value }) => {
   const { calc, setCalc } = useContext(CalcContext);
-  
+
   // user click comma
   const commaClick = () => {
     setCalc({
       ...calc,
-      num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
-    })
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
   };
 
   // user click C
 
   const resetClick = () => {
-    setCalc({ sign: '', num: 0, res: 0})
-  }
+    setCalc({ sign: "", num: 0, res: 0 });
+  };
   // user click number
   const handleClickButton = () => {
-    const numberString = value.toString()
+    const numberString = value.toString();
 
     let numberValue;
-    if (numberString === '0' && calc.num === 0 ) {
-      numberValue = "0"
-
+    if (numberString === "0" && calc.num === 0) {
+      numberValue = "0";
     } else {
-      numberValue = Number(calc.num + numberString)
+      numberValue = Number(calc.num + numberString);
     }
 
     setCalc({
       ...calc,
-      num: numberValue
-    })
-  }
+      num: numberValue,
+    });
+  };
   // user click opt
 
   const signClick = () => {
     setCalc({
       sign: value,
       res: !calc.res && calc.num ? calc.num : calc.res,
-      num: 0
+      num: 0,
+    });
+  };
+  // user click =
+  const equalsClick = () => {
+    if (calc.res && calc.num) {
+      const math = (a, b, sign) => {
+        const result = {
+          "+": (a, b) => a + b,
+          "-": (a, b) => a - b,
+          x: (a, b) => a * b,
+          "/": (a, b) => a / b,
+        };
+       return result[sign](a, b);
+      };
+      setCalc({
+        res: math(calc.res, calc.num, calc.sign),
+        sign: "",
+        num: 0,
+      });
+    }
+  };
+
+  // user click %
+
+  const persentClick = () => {
+    setCalc({
+      num: (calc.num / 100),
+      res: (calc.res / 100),
+      sign: ''
     })
   }
 
+  // user click invert button
+
+  const invertClick = () => {
+    setCalc({
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: ''
+    })
+  }
   const handleBtnClick = () => {
     const results = {
       ".": commaClick,
-      'C': resetClick,
-      '/': signClick,
-      'x': signClick,
-      '-': signClick,
-      '+': signClick,
-
+      "C": resetClick,
+      "/": signClick,
+      "x": signClick,
+      "-": signClick,
+      "+": signClick,
+      "=": equalsClick,
+      "%": persentClick,
+      "+-": invertClick
     };
-    if(results[value]) {
-      return results[value]()
+    if (results[value]) {
+      return results[value]();
     } else {
-      return handleClickButton()
+      return handleClickButton();
     }
   };
   return (
