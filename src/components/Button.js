94b05d308f8998c0dc87a87746eb422245bc4c79
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-05-16 01:16:16 +0500

    add functions

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,82 @@
-import React from 'react'
-const getStyleName = btn => {
+import React, { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
+const getStyleName = (btn) => {
   const className = {
-    '=': 'equals',
-    'x': 'opt',
-    '-': 'opt',
-    '+': 'opt',
-    '/': 'opt',
-  }
-  return className[btn]
-}
+    "=": "equals",
+    x: "opt",
+    "-": "opt",
+    "+": "opt",
+    "/": "opt",
+  };
+  return className[btn];
+};
 const Button = ({ value }) => {
-  return (
-    <div className={`${getStyleName(value)} button `}>{value}</div>
-  )
-}
+  const { calc, setCalc } = useContext(CalcContext);
+  
+  // user click comma
+  const commaClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
+    })
+  };
 
-export default Button
+  // user click C
+
+  const resetClick = () => {
+    setCalc({ sign: '', num: 0, res: 0})
+  }
+  // user click number
+  const handleClickButton = () => {
+    const numberString = value.toString()
+
+    let numberValue;
+    if (numberString === '0' && calc.num === 0 ) {
+      numberValue = "0"
+
+    } else {
+      numberValue = Number(calc.num + numberString)
+    }
+
+    setCalc({
+      ...calc,
+      num: numberValue
+    })
+  }
+  // user click opt
+
+  const signClick = () => {
+    setCalc({
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0
+    })
+  }
+
+  const handleBtnClick = () => {
+    const results = {
+      ".": commaClick,
+      'C': resetClick,
+      '/': signClick,
+      'x': signClick,
+      '-': signClick,
+      '+': signClick,
+
+    };
+    if(results[value]) {
+      return results[value]()
+    } else {
+      return handleClickButton()
+    }
+  };
+  return (
+    <button
+      onClick={handleBtnClick}
+      className={`${getStyleName(value)} button `}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Button;
